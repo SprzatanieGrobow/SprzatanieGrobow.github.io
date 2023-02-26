5d1df6abaf703df2e782aef83a0d9ac8b4ecda81
sprzataniegrobow <48531220+sprzataniegrobow@users.noreply.github.com>
2023-02-26 18:22:14 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function home() {
+    var elemen = document.getElementById("home");
+    elemen.scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"});
+  }
+function uslugi() {
+    var elemen = document.getElementById("uslugi");
+    elemen.scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"});
+  }
+function about() {
+    var elemen = document.getElementById("about");
+    elemen.scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"});
+  }
+function kont() {
+    var elemen = document.getElementById("kont");
+    elemen.scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"});
+  }
